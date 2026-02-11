0b5b1ec93785b60f881b220bb7f70863a8e3386f
Denislav <denyslav.dimitrov@gmail.com>
2026-02-11 22:03:22 +0200

    space.packet.protocol * removed previous implementation which doesn't have spring boot + maven + JUnit tests + missing logic

--- CCSDS-SpacePacketProtocol-Implementation/src/spp/services/octetStringService/OctetStringListener.java
+++
@@ -1,9 +0,0 @@
-package spp.services.octetStringService;
-
-/*
-  OctetStringListener class contains method indication which represents the primitive PACKET.indication
- */
-@FunctionalInterface
-public interface OctetStringListener {
-  void indication(byte[] data);
-}
--- CCSDS-SpacePacketProtocol-Implementation/src/spp/services/octetStringService/OctetStringService.java
+++
@@ -1,9 +0,0 @@
-package spp.services.octetStringService;
-
-/*
-  OctetStringService class contains method request which represents the primitive PACKET.request
- */
-public interface OctetStringService {
-  void request(byte[] data);
-  void setListener(OctetStringListener octetStringListener);
-}
